ff5da796f405924c66bf9b6eb8caff7c90aa012c
Roberto Estrada <restrada@paridin.com>
2026-07-03 21:43:01 -0600

    chore(gradle): centralize android plugin classpath

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    kotlin("android") apply false
+    kotlin("jvm") apply false
+    id("org.jetbrains.kotlin.plugin.compose") apply false
+}
